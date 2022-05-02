82effb0862aa8c05cba4e62a4a84b63c6cc2ed02
Thomas Nicollet <thomas.nicollet@nebulis.io>
2022-05-02 23:10:43 +0200

    :sparkles: Added CRUD for tickets

--- backend/backend-svc/src/main.rs
+++ backend/backend-svc/src/main.rs
@@ -1,22 +1,45 @@
-use std::net::SocketAddr;
+use std::{net::SocketAddr, str::FromStr};
 
-use axum::Router;
-use mongodb::{Client, options::ClientOptions};
+use axum::{
+    extract::Path,
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, post, delete},
+    Extension, Json, Router,
+};
+use futures::StreamExt;
+use mongodb::{bson::{oid::ObjectId, doc}, options::ClientOptions, Client};
 use once_cell::sync::Lazy;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+use serde_json::json;
 use tracing::log;
 
-
-static CONFIG: Lazy<Config> = Lazy::new(|| {
-    match envy::from_env::<Config>() {
-        Ok(config) => config,
-        Err(error) => panic!("{:#?}", error)
-    }
+static CONFIG: Lazy<Config> = Lazy::new(|| match envy::from_env::<Config>() {
+    Ok(config) => config,
+    Err(error) => panic!("{:#?}", error),
 });
 
 #[derive(Deserialize)]
 struct Config {
-    mongo_host: String
+    mongo_host: String,
+    mongo_database: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Ticket {
+    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+    id: Option<ObjectId>,
+    title: String,
+    content: String,
+}
+
+impl Ticket {
+    pub fn with_id(self) -> Self {
+        Self {
+            id: self.id.or_else(|| Some(ObjectId::new())),
+            ..self
+        }
+    }
 }
 
 #[tokio::main]
@@ -27,13 +50,16 @@ async fn main() -> anyhow::Result<()> {
 
     // Manually set an option.
     client_options.app_name = Some("Backend Service".to_string());
-    
+
     // Get a handle to the deployment.
-    let _client = Client::with_options(client_options)?;
-    
+    let client = Client::with_options(client_options)?;
 
     // build our application with a route
-    let app = Router::new();
+    let app = Router::new()
+        .route("/tickets", get(get_tickets))
+        .route("/tickets", post(add_ticket))
+        .route("/tickets/:mongo_id", delete(delete_ticket))
+        .layer(Extension(client));
 
     // run our app with hyper
     // `axum::Server` is a re-export of `hyper::Server`
@@ -45,4 +71,74 @@ async fn main() -> anyhow::Result<()> {
         .unwrap();
 
     Ok(())
+}
+
+async fn delete_ticket(
+    Extension(mongodb): Extension<Client>,
+    Path(ticket_id): Path<String>,
+) -> impl IntoResponse {
+    let data = mongodb
+        .database(&CONFIG.mongo_database)
+        .collection::<Ticket>("tickets")
+        .delete_one(
+            doc! {
+                "_id": ObjectId::from_str(&ticket_id).expect("Couldn't transform OID")
+            },
+            None,
+        )
+        .await;
+
+    if let Ok(data) = data {
+
+        log::debug!("{data:?}");
+
+        (StatusCode::OK, Json(json!({})))
+    } else {
+        let why = data.unwrap_err().to_string();
+        (
+            StatusCode::NOT_FOUND,
+            Json(json!({ "error": why })),
+        )
+    }
+}
+
+async fn get_tickets(Extension(mongodb): Extension<Client>) -> impl IntoResponse {
+    let data = mongodb
+        .database(&CONFIG.mongo_database)
+        .collection::<Ticket>("tickets")
+        .find(None, None)
+        .await;
+
+    if let Ok(tickets) = data {
+        let tickets = tickets
+            .filter_map(|x| async move { x.ok() })
+            .collect::<Vec<Ticket>>()
+            .await;
+
+        (StatusCode::OK, Json(json!({ "tickets": tickets })))
+    } else {
+        let why = data.unwrap_err().to_string();
+
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json(json!({ "error": why })),
+        )
+    }
+}
+
+async fn add_ticket(
+    Json(ticket): Json<Ticket>,
+    Extension(mongodb): Extension<Client>,
+) -> impl IntoResponse {
+    let ticket_to_insert = ticket.with_id();
+
+    let data = mongodb
+        .database(&CONFIG.mongo_database)
+        .collection::<Ticket>("tickets")
+        .insert_one(&ticket_to_insert, None)
+        .await;
+
+    log::debug!("Inserting ticket {data:?}");
+
+    (StatusCode::OK, Json(ticket_to_insert))
 }
